3da8c51fba4dfcbdb9ea4703674534f6474d2758
Pablerdo <pablosalamanca88@gmail.com>
2022-09-03 12:00:18 -0400

    working prototype

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-import {Button, IconButton} from "@mui/material";
 
 function App() {
   return (
@@ -11,23 +10,23 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Pablo Salamanca</h1>
         <h4 className="App-text">Computer Science @ University of Pennsylvania</h4>
-        <h4 className="App-text">Curriculum Vitae</h4>
+        <h4 className="App-text">CV</h4>
         <div className="App-icon-bar">
-          <IconButton target='_blank' href='https://github.com/Pablerdo'>
+          <a className='App-icon-link' target='_blank' href='https://github.com/Pablerdo' rel='noreferrer'>
             <GitHubIcon />
-          </IconButton>
-          <IconButton target='_blank' href='https://www.linkedin.com/in/pablo-salamanca-97a848134/'>
+          </a>
+          <a className='App-icon-link' target='_blank' href='https://www.linkedin.com/in/pablo-salamanca-97a848134/' rel='noreferrer'>
             <LinkedInIcon />
-          </IconButton>
-          <IconButton target='_blank' href='https://www.youtube.com/channel/UC8zVlsl3olsvZrFF8P1vGkw'>
+          </a>
+          <a className="App-icon-link" target='_blank' href='https://www.youtube.com/channel/UC8zVlsl3olsvZrFF8P1vGkw' rel='noreferrer'>
             <YouTubeIcon />
-          </IconButton>
-          <a target='_blank' href='https://create.arduino.cc/projecthub/Pablerdo/wireless-laser-gate-timing-system-for-track-and-field-ba8cd9?ref=search&ref_id=track%20and%20field&offset=2' >
+          </a>
+          <a className='App-icon-link' target='_blank' href='https://create.arduino.cc/projecthub/Pablerdo/wireless-laser-gate-timing-system-for-track-and-field-ba8cd9?ref=search&ref_id=track%20and%20field&offset=2' rel='noreferrer'>
             <img src={require('./assets/arduino_logo.png')} alt="logo" height='20' />
           </a>
-          <IconButton target='_blank' href='https://twitter.com/Pablerdo'>
+          <a className='App-icon-link' target='_blank' href='https://twitter.com/Pablerdo' rel='noreferrer'>
             <TwitterIcon />
-          </IconButton>
+          </a>
         </div>
       </header>
     </div>
